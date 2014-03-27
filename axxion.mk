8b999096618b6055a8835c67ac165e5bb449af52
Ivan Martinez <ivanthegreat15@gmail.com>
2014-03-27 11:08:45 -0400

    Axxionize

--- axxion.mk
+++ axxion.mk
@@ -1,21 +1,21 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/axxion/config/cdma.mk)
 
 TARGET_KERNEL_CONFIG := gee_defconfig
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-$(call inherit-product, vendor/cm/config/common.mk)
+# Inherit some common axxion stuff.
+$(call inherit-product, vendor/axxion/config/common_full_phone.mk)
+$(call inherit-product, vendor/axxion/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/lge/gee/cm_gee.mk)
+$(call inherit-product, device/lge/gee/axxion_gee.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gee
-PRODUCT_NAME := cm_gee
+PRODUCT_NAME := axxion_gee
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-gee
 PRODUCT_MANUFACTURER := LGE
